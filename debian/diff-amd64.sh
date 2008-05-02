2f81d6770ecd4fa6e4e0f463481379a863f2a21d
Ove Kaaven <ovek@arcticnet.no>
2008-05-02 05:19:16 +0200

    Release 0.9.49-1

---
+++ debian/diff-amd64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DEBIAN_VERSION=$(dpkg-parsechangelog|sed -n 's,^Version: \(.*\)$,\1,p')
+NATIVE_VERSION=$(echo $DEBIAN_VERSION|sed -n 's,^\(.*\)-[^-]*$,\1,p')
+DIR=wine-$NATIVE_VERSION
+DIFFNAME=wine_$DEBIAN_VERSION.diff
+DSCNAME=wine_$DEBIAN_VERSION.dsc
+(
+cd ..
+echo Updating $DIFFNAME.gz...
+gunzip $DIFFNAME
+filterdiff -x "*/debian/amd64.tar.lzma.uu" $DIFFNAME > wine-temp.diff
+diff -uN $DIR.orig/debian/amd64.tar.lzma.uu $DIR/debian/amd64.tar.lzma.uu >> wine-temp.diff
+mv wine-temp.diff $DIFFNAME
+gzip -9 $DIFFNAME
+echo Updating $DSCNAME...
+MD5=$(md5sum $DIFFNAME.gz|sed -n 's,^\([^ ]*\).*$,\1,p')
+SIZE=$(ls -l $DIFFNAME.gz|sed -n 's,^[^ ]* [^ ]* [^ ]* [^ ]* \([^ ]*\).*$,\1,p')
+head -n -1 $DSCNAME > wine-temp.dsc
+echo " $MD5 $SIZE $DIFFNAME.gz" >> wine-temp.dsc
+mv wine-temp.dsc $DSCNAME
+)
