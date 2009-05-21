66dd3dbffe2eee938b897a12289e9fdb53ac2ed3
Ove Kaaven <ovek@arcticnet.no>
2009-05-21 14:56:55 +0200

    Removed *.dirs files, rely on dh_install (and split.sh) to create directories as needed.

--- debian/split.sh
+++ debian/split.sh
@@ -3,7 +3,10 @@ SRC="$1"
 PREFIX="$2"
 while read DEST NAME; do
   for bin in debian/$SRC/$PREFIX/$NAME; do
-    [ ! -f $bin ] || mv $bin debian/$DEST/$PREFIX
+    if [ -f $bin ]; then
+      install -d debian/$DEST/$PREFIX
+      mv $bin debian/$DEST/$PREFIX
+    fi
   done
 done < debian/$SRC.split
 # return success
