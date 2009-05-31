089c17840b89117256bc16e6872e220e7877acd7
Ove Kaaven <ovek@arcticnet.no>
2009-06-01 00:18:26 +0200

    On amd64, winelib is now installed into /usr/lib32 instead of /usr/lib. (This *is* an upload to experimental, after all...)

--- debian/prep-install.sh
+++ debian/prep-install.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
-for inst in debian/*.install-common; do
-  package="$(basename "$inst" .install-common)"
-  cp debian/$package.install-common debian/$package.install
-  # certain binaries are only compiled on some platforms;
-  # if they were compiled on the current one, install them
-  for bin in $(cat debian/$package.install-platform); do
+LIBDIRS="$1"
+
+function expand_common
+{
+  sed "s,/usr/lib,/usr/$1," debian/$package.install-common > debian/$package.install
+  shift
+  while [ -n "$1" ]; do
+    sed -n "s,/usr/lib,/usr/$1,p" debian/$package.install-common >> debian/$package.install
+    shift
+  done
+}
+
+# certain binaries are only compiled on some platforms;
+# if they were compiled on the current one, install them
+function expand_platform
+{
+  if [ ! -f debian/$package.install-platform ]; then
+    return
+  fi
+  for bin in $(sed "s,/usr/lib,/usr/$1," debian/$package.install-platform); do
     [ ! -f $bin ] || echo $bin >> debian/$package.install
   done
+  shift
+  while [ -n "$1" ]; do
+    for bin in $(sed -n "s,/usr/lib,/usr/$1,p" debian/$package.install-platform); do
+      [ ! -f $bin ] || echo $bin >> debian/$package.install
+    done
+    shift
+  done
+}
+
+for inst in debian/*.install-common; do
+  package="$(basename "$inst" .install-common)"
+  expand_common $LIBDIRS
+  expand_platform $LIBDIRS
 done
+
 # return success
 true
--- debian/split.sh
+++ debian/split.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 SRC="$1"
-PREFIX="$2"
-while read DEST NAME; do
-  for bin in debian/$SRC/$PREFIX/$NAME; do
-    if [ -f $bin ]; then
-      install -d debian/$DEST/$PREFIX
-      mv $bin debian/$DEST/$PREFIX
-    fi
-  done
-done < debian/$SRC.split
+shift
+while [ -n "$1" ]; do
+  PREFIX="$1"
+  while read DEST NAME; do
+    for bin in debian/$SRC/$PREFIX/$NAME; do
+      if [ -f $bin ]; then
+        install -d debian/$DEST/$PREFIX
+        mv $bin debian/$DEST/$PREFIX
+      fi
+    done
+  done < debian/$SRC.split
+  shift
+done
 # return success
 true
