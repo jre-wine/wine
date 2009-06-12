168d6314d53558b864c6bc0d8045df165b6b762b
Ove Kaaven <ovek@arcticnet.no>
2009-06-12 15:51:38 +0200

    Fix cleanup after building a package with a suffix and extra dependencies.

--- debian/cleandeps.sh
+++ debian/cleandeps.sh
@@ -4,7 +4,7 @@ shift
 echo Cleaning extra dependencies...
 for dep in debian/*.deps; do
   package="$(basename "$dep" .deps)"
-  path="debian$SUFFIX/$package"
+  path="debian/$package$SUFFIX"
 #  echo $path
   rm -f "$path/extradep32" "$path/extradep64"
 done
