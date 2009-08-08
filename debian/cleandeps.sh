24f431d67e3a77d86b552a2c0b64307e79967a31
Ove Kaaven <ovek@arcticnet.no>
2009-08-08 22:33:20 +0200

    Added a script to generate Recommends lines. Initially use it to make libwine recommend the libgsm package Wine was compiled against. (The package splitting has gone far enough, I don't think I want to make a separate libwine-codecs package to depend on libgsm. I'm just going to use Recommends instead for stuff like that. Perhaps even eliminate some more libwine-* packages in the future...)

--- debian/cleandeps.sh
+++ debian/cleandeps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 SUFFIX="$1"
-shift
-echo Cleaning extra dependencies...
-for dep in debian/*.deps; do
-  package="$(basename "$dep" .deps)"
-  path="debian/$package$SUFFIX"
-#  echo $path
-  rm -f "$path/extradep32" "$path/extradep64"
+
+for ext in deps recs; do
+  for dep in debian/*.${ext}; do
+    package="$(basename "$dep" .${ext})"
+    path="debian/$package$SUFFIX"
+  #  echo $path
+    rm -f "$path/extra${ext}32" "$path/extra${ext}64"
+  done
 done
+
 # return success
 true
--- debian/gendeps.sh
+++ debian/gendeps.sh
@@ -2,15 +2,18 @@
 SUFFIX="$1"
 shift
 ARCHS="$@"
-echo Forcing extra dependencies...
-for dep in debian/*.deps; do
-  package="$(basename "$dep" .deps)"
-  path="debian/$package$SUFFIX"
-  deplist=$(sed 's,^,-l,' $dep)
-#  echo $path: $deplist
-  for arch in $ARCHS; do
-    gcc -m$arch -Wl,--noinhibit-exec -o "$path/extradep$arch" debian/extradep.c $deplist
+
+for ext in deps recs; do
+  for dep in debian/*.${ext}; do
+    package="$(basename "$dep" .${ext})"
+    path="debian/$package$SUFFIX"
+    deplist=$(sed 's,^,-l,' $dep)
+  #  echo $path: $deplist
+    for arch in $ARCHS; do
+      gcc -m$arch -Wl,--noinhibit-exec -o "$path/extra${ext}$arch" debian/extradep.c $deplist
+    done
   done
 done
+
 # return success
 true
