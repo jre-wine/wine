cd5cef99941778c485ad7116bc55e6ae651ac79d
Hilko Bengen <bengen@debian.org>
2012-07-07 17:12:14 +0200

    wine64-bin: Use xmessage (with fallback to console) for multiarch instructions

--- debian/wine64.sh
+++ debian/wine64.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-arch=$(dpkg --print-architecture | sed s/amd64/i386/)
+XMESSAGE=/usr/bin/xmessage
+ARCH="`dpkg --print-architecture`"
+ARCH_I386="`echo $ARCH | sed s,amd64,i386,`"
 
-echo "This is the wine64-bin helper package, which does not provide wine itself,"
-echo "but instead exists solely to provide the following information about"
-echo "enabling multiarch on your system in order to be able to install and run"
-echo "the 32-bit wine packages."
-echo ""
-echo "The following commands should be issued as root or via sudo in order to"
-echo "enable multiarch (the last command installs 32-bit wine):"
-echo ""
-echo "  # dpkg --add-architecture $arch"
-echo "  # apt-get update"
-echo "  # apt-get install wine-bin:$arch"
-echo ""
-echo "Depending on which Debian release is present on this system, the development"
-echo "version of wine may be available, which if available can be installed with:"
-echo ""
-echo "  # apt-get install wine-bin-unstable:$arch"
-echo ""
-echo "Note that this package (wine64-bin) will be removed in the process.  For"
-echo "more information on the multiarch conversion, see:"
-echo "http://wiki.debian.org/Multiarch/HOWTO"
+TITLE="Debian / Wine: Multiarch Instructions"
+
+I386_MSG=\
+"This is the wine64-bin helper package, which does not provide wine itself,
+but instead exists solely to provide the following information about
+enabling multiarch on your system in order to be able to install and run
+the 32-bit wine packages.
+
+The following commands should be issued as root or via sudo in order to
+enable multiarch (the last command installs 32-bit wine):
+
+  # dpkg --add-architecture ${ARCH_I386}
+  # apt-get update
+  # apt-get install wine-bin:${ARCH_I386}
+
+Be very careful as spaces matter above.  Note that this package
+(wine64-bin) will be removed in the process.  For more information on
+the multiarch conversion, see: http://wiki.debian.org/Multiarch/HOWTO"
+
+$XMESSAGE -center \
+    -buttons ok:0 -default ok \
+    -title "$TITLE" \
+    "$I386_MSG" 2>/dev/null
+notify=$?
+if [ $notify -eq 1 ] ; then
+    # xmessage was unable to notify the user, try tty instead
+    echo "** $TITLE **"
+    echo "" >&2
+    echo "$I386_MSG" >&2
+    echo "" >&2
+    echo -n "(okay) " >&2
+    read confirm
+fi
+
+exit 1
